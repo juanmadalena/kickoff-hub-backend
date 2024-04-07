f7fc1c8c62199618ce90500abe8da8b6ac039352
Juan Madalena <juanmadalena06@gmail.com>
2024-04-07 04:51:49 +0200

    fix! create match

--- src/domain/dtos/match/create-match.dto.ts
+++ src/domain/dtos/match/create-match.dto.ts
@@ -1,8 +1,9 @@
+import { CustomError } from "../../../config"
 import { checkDate } from "../../../utils/checkDate"
 
 export class CreateMatchDto{
     private constructor(
-        public readonly date: Date,
+        public readonly date: Date | String,
         public readonly time: string,
         public readonly duration: string,
         public readonly description: string,
@@ -17,44 +18,46 @@ export class CreateMatchDto{
     ){}
 
     
-    static create(props: {[key:string]:any}): [string?, CreateMatchDto?] {
+    static create(props: {[key:string]:any}): [CustomError?, CreateMatchDto?] {
         
         const { date, time, duration, description, location, address, idAddress, minPlayers, maxPlayers, idOrganizer, isPrivate = false } = props
-        // console.log('props', props)
+
         // Date
-        if(!date) return ['Date is required']
+        if(!date) return [{message:'Date is required', input:'date'}]
 
         // Time
-        if(!time) return ['Time is required']
+        if(!time) return [{message:'Time is required', input:'time'}]
 
         // Duration
-        if(!duration) return ['Duration is required']
+        if(!duration) return [{message:'Duration is required', input:'duration'}]
 
         // Description
         // if(!description) return ['Description is required']
 
         // Location
-        if(!location) return ['Location is required']
+        if(!location) return [{message:'Location is require', input:'location'}]
 
         //address
-        if(!address) return ['Address is required']
+        if(!address) return [{message:'Location is incorrect', input:'location'}]
 
         // Id Address
-        if(!idAddress) return ['Id Address is required']
+        if(!idAddress) return [{message:'Location is incorrect', input:'location'}]
 
         // Min Players
-        if(!minPlayers) return ['Min Players is required']
+        if(!minPlayers) return [{message:'Min players is required', input:'minPlayers'}]
 
         // Max Players
-        if(!maxPlayers) return ['Max Players is required']
+        if(!maxPlayers) return [{message:'Max players is required', input:'maxPlayers'}]
+
+        if(minPlayers > maxPlayers) return [{message:'Min players must be less than max players', input:'minPlayers'}]
 
         // Price
         // if(!price) return ['Price is required']
 
         // Id Organizer
-        if(!idOrganizer) return ['Id Organizer is required']
+        if(!idOrganizer) return [{message:'Id organizer is required', input:'organizer'}]
 
-        if(checkDate(date, time)) return ['Invalid date or time']
+        if(!checkDate(date, time)) return [{message:'Match is in the past', input:'date'}]
 
         return [undefined, new CreateMatchDto(date, time, duration, description, location, address, idAddress, minPlayers, maxPlayers, idOrganizer, isPrivate)]
     }
--- src/utils/checkDate.ts
+++ src/utils/checkDate.ts
@@ -1,17 +1,22 @@
 export const checkDate = ( date: Date, time:string ): boolean => {
     
     if(!date || !time) return false
-    console.log('date', date, time)
-    const dateFormated = new Date(date)
 
-    const day = dateFormated.getDate()
-    const month = dateFormated.getMonth()+1
-    const year = dateFormated.getFullYear()
+    if(typeof date === 'string') date = new Date(date)
+        
+    // Match Date
+    const [ dateFormated ] = new Date(date).toISOString().split('T')
+    const dateTime = new Date(`${dateFormated}T${time}:00.000Z`)
 
-    const dateTime = new Date(`${year}-${month}-${day} ${time}`)
-    console.log('dateTime', dateTime, new Date())
-    console.log('dateTime < new Date()', dateTime < new Date())
-    if(dateTime < new Date()) return false
+    // Date in Ireland
+    const currentDateIreland = new Date().toLocaleDateString('en-IE', {timeZone:'Europe/Dublin', day:'2-digit', month:'2-digit', year:'numeric',hour:'2-digit', minute:'2-digit', second:'2-digit'})
+    const [ currentDate, currentTime ] = currentDateIreland.split(',')
+    const [ day, month, year ] = currentDate.split('/')
+
+    const currentDateFormatted = new Date(`${year}-${month}-${day}T${currentTime.trim()}.000Z`)
+
+    // Verify if the date is in the past
+    if(dateTime.getTime() < currentDateFormatted.getTime()) return false
 
     return true
 }
